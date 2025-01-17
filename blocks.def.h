ca5e696815d1f8e722de99ae3f5620db35e8f15d
zekar <>
2025-01-17 15:19:41 -0600

    a

--- blocks.def.h
+++ blocks.def.h
@@ -1,10 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+  //{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
-};
+  {"<\x01  ", "date +'%a %d/%b/Y %H:%M:%S%P'", 1, 0}
+}; 
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
 static char delim[] = " | ";
